98eaf8b0af17b371f682e4cf8f23c45cba767511
Piotr Bączek <pgolasz@gmail.com>
2026-07-25 17:41:06 +0200

    init

---
+++ tests/BitboardTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use piotrbaczek\ChessEngine\Bitboard;
+
+class BitboardTest extends TestCase
+{
+    public function testBitboard()
+    {
+        $bitboard = new Bitboard();
+        $this->assertInstanceOf(Bitboard::class, $bitboard);
+    }
+}
